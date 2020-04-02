7c598787aa76610ea5985c0b8256feeebf05f9ea
Rozmarynowski <rozmarynowski_adam@onet.pl>
2020-04-02 03:55:54 +0200

    Model making
    Not working

--- app/src/main/java/com/example/flightapp/JsonFetch/JsonFetch.kt
+++ app/src/main/java/com/example/flightapp/JsonFetch/JsonFetch.kt
@@ -1,7 +1,12 @@
 package com.example.flightapp.JsonFetch
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import okhttp3.*
 import java.io.IOException
+import java.util.*
+import kotlin.collections.ArrayList
 
 class JsonFetch {
 
@@ -17,9 +22,23 @@ class JsonFetch {
             override fun onResponse(call: Call, response: Response) {
                 val body = response.body?.string()
                 println(body)
-            }
+
+                val gson = GsonBuilder().create()
+                val allFlights = gson.fromJson(body, AllFlights::class.java)
+
+            } //  <-- TU STAWIAM KROPKĘ DEBUGGERA
 
         })
     }
-
 }
+
+
+// KLASY WRZUCIŁEM NA RAZIE TU, EBY BYŁO ŁATWIEJ
+class AllFlights(val states:List<List<State>>) { // <-- JEŻELI TU DAMY STRING ZAMIAST STATE, TO DA JAKIES WYNIKI, ALE ZLE
+}
+
+class State(val longitude: Float, val latitude: Float){
+}
+
+
+

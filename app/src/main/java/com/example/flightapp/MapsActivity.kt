a64656cc325e8515bbd8d491f458f39a76bc556d
Piotrek1697 <30784285+Piotrek1697@users.noreply.github.com>
2020-04-03 20:34:29 +0200

    Searching and put all flights button added

--- app/src/main/java/com/example/flightapp/MapsActivity.kt
+++ app/src/main/java/com/example/flightapp/MapsActivity.kt
@@ -1,11 +1,15 @@
 package com.example.flightapp
 
 import android.animation.ObjectAnimator
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.ScaleAnimation
+import android.view.inputmethod.InputMethodManager
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flightapp.InfoWindow.CustomInfoWindowForGoogleMap
 import com.example.flightapp.JsonFetch.JsonFetch
@@ -18,7 +22,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-
+import java.util.*
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -32,8 +36,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.supportActionBar!!.hide()
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -43,6 +47,49 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         cordsList = JsonFetch.fetchJson()
 
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                query.trim()
+                if (query != ""){
+                    var hasLetters = false
+                    var hasNumbers = false
+                    query.forEach {
+                        if (it.isLetter())
+                            hasLetters = true
+                        if (it.isDigit())
+                            hasNumbers = true
+                    }
+                    //Searching by flight number
+                    if (hasLetters && hasNumbers){
+                        mMap.clear()
+                        val list = cordsList.filter {it.callsign == query.toUpperCase()} as MutableList<State>
+                        if (list.size != 0)
+                            putMarkersOnMap(mMap, list)
+                        else
+                            Toast.makeText(applicationContext, "There is no flights with number: ${query}", Toast.LENGTH_SHORT).show()
+                    }else if(isCountry(query)){ //Searching by country
+                        mMap.clear()
+                        val list = cordsList.filter {it.origin_country.toLowerCase() == query.toLowerCase()} as MutableList<State>
+                        if (list.size != 0)
+                            putMarkersOnMap(mMap, list)
+                        else
+                            Toast.makeText(applicationContext, "There is no flights with country: ${query}", Toast.LENGTH_SHORT).show()
+                    }else
+                        Toast.makeText(applicationContext, "Wrong query string", Toast.LENGTH_SHORT).show()
+                }
+                appearAnimation(searchView, searchButton)
+
+                searchView.setQuery("", false);
+                searchView.clearFocus()
+                return false
+            }
+
+        })
     }
 
     /**
@@ -56,37 +103,55 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        val wroclawCords = LatLng(51.1078852, 17.0385376)
+        putMarkersOnMap(mMap, cordsList)
+        //Set center on Wroclaw
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(wroclawCords, 4.8f))
+    }
+
+    private fun putMarkersOnMap(mMap: GoogleMap, statesList: MutableList<State>) {
         val plane = AirplaneVectorMarkers()
-        val wroclawCords = LatLng(51.1078852,17.0385376)
-        cordsList.forEach {
+        statesList.forEach {
             val cord = LatLng(it.latitude, it.longitude)
             mMap.addMarker(
                 MarkerOptions().title(it.callsign)
-                    .snippet("Country: ${it.origin_country}\n" +
-                            "Altitude ${it.geo_altitude.toInt()} m\n" +
-                            "Velocity: ${((it.velocity * 3.6).toInt()).toString()} km/h")
-                    .position(cord).rotation(it.true_track).icon(
-                    plane.vectorMapDescriptor(
-                        applicationContext,
-                        R.drawable.ic_flight_black_24dp
+                    .snippet(
+                        "Country: ${it.origin_country}\n" +
+                                "Altitude ${it.geo_altitude.toInt()} m\n" +
+                                "Velocity: ${((it.velocity * 3.6).toInt()).toString()} km/h"
                     )
-                )
+                    .position(cord).rotation(it.true_track).icon(
+                        plane.vectorMapDescriptor(
+                            applicationContext,
+                            R.drawable.ic_flight_black_24dp
+                        )
+                    )
             )
             mMap.setInfoWindowAdapter(CustomInfoWindowForGoogleMap(this))
         }
-        //Set center on Wroclaw
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(wroclawCords, 4.8f))
-        
+    }
+
+    fun allFlights(view: View){
+        mMap.clear()
+        putMarkersOnMap(mMap,cordsList)
     }
 
     fun searchButtonAction(view: View) {
-        ObjectAnimator.ofFloat(searchButton, View.ALPHA, 1f, 0f).setDuration(1000).start()
-        //searchButton.visibility = View.INVISIBLE
-        searchButton.isClickable = false
+        appearAnimation(view, searchView)
+        showSoftKeyboard(searchView)
+    }
 
-        searchView.isClickable = true
-        searchView.visibility = View.VISIBLE
-        ObjectAnimator.ofFloat(searchView, View.ALPHA, 0f, 1f).setDuration(500).start()
+    fun searchViewAction(view: View) {
+        searchView.clearFocus()
+    }
+
+    private fun appearAnimation(disappearView: View, appearView: View) {
+        ObjectAnimator.ofFloat(disappearView, View.ALPHA, 1f, 0f).setDuration(1000).start()
+        disappearView.isClickable = false
+
+        appearView.isClickable = true
+        appearView.visibility = View.VISIBLE
+        ObjectAnimator.ofFloat(appearView, View.ALPHA, 0f, 1f).setDuration(500).start()
         val anim = ScaleAnimation(
             0.0f,
             1.0f,
@@ -98,30 +163,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             0.5f
         )
         anim.duration = 700
-        searchView.startAnimation(anim)
-}
-
-    fun searchViewAction(view: View){
-        ObjectAnimator.ofFloat(searchView, View.ALPHA, 1f, 0f).setDuration(700).start()
-        searchView.visibility = View.INVISIBLE
-        searchView.isClickable = false
-
-        searchButton.visibility = View.VISIBLE
-        ObjectAnimator.ofFloat(searchButton, View.ALPHA, 0f, 1f).setDuration(1000).start()
-        val anim = ScaleAnimation(
-            1.0f,
-            0.0f,
-            1.0f,
-            1.0f,
-            Animation.RELATIVE_TO_SELF,
-            0.0f,
-            Animation.RELATIVE_TO_SELF,
-            0.5f
-        )
-        anim.duration = 700
-        searchView.startAnimation(anim)
-        searchButton.isClickable = true
+        appearView.startAnimation(anim)
     }
 
+    private fun showSoftKeyboard(view: View) {
+        if (view.requestFocus()) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+        }
+    }
 
+    private fun isCountry(string: String) : Boolean{
+        var isCountry = false
+        val l = Locale.ENGLISH
+        Locale.getISOCountries().forEach {
+            val locale = Locale("",it)
+            val country = locale.getDisplayCountry(l)
+            if (string.toLowerCase() == country.toLowerCase())
+                isCountry = true
+        }
+        return isCountry
+    }
 }

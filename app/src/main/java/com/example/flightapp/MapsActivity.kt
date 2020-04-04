0f0084ed957df4f343d252e003727d661ee54502
Piotrek1697 <30784285+Piotrek1697@users.noreply.github.com>
2020-04-04 17:53:00 +0200

    Filter with another thread connected

--- app/src/main/java/com/example/flightapp/MapsActivity.kt
+++ app/src/main/java/com/example/flightapp/MapsActivity.kt
@@ -38,6 +38,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var searchView: SearchView
     private lateinit var searchButton: FloatingActionButton
     private lateinit var cordsList: MutableList<State>
+    private var isFiltred: Boolean = false
+    private lateinit var filterQuery: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,8 +53,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         searchView = findViewById(R.id.searchView)
         searchButton = findViewById(R.id.searchActionButton)
 
-        cordsList = JsonFetch.fetchJson()
-
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
             override fun onQueryTextChange(newText: String): Boolean {
@@ -60,38 +60,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
 
             override fun onQueryTextSubmit(query: String): Boolean {
-                query.trim()
-                if (query != ""){
-                    var hasLetters = false
-                    var hasNumbers = false
-                    query.forEach {
-                        if (it.isLetter())
-                            hasLetters = true
-                        if (it.isDigit())
-                            hasNumbers = true
-                    }
-                    //Searching by flight number
-                    if (hasLetters && hasNumbers){
-                        mMap.clear()
-                        val list = cordsList.filter {it.callsign == query.toUpperCase()} as MutableList<State>
-                        if (list.size != 0)
-                            putMarkersOnMap(mMap, list)
-                        else
-                            Toast.makeText(applicationContext, "There is no flights with number: ${query}", Toast.LENGTH_SHORT).show()
-                    }else if(isCountry(query)){ //Searching by country
-                        mMap.clear()
-                        val list = cordsList.filter {it.origin_country.toLowerCase() == query.toLowerCase()} as MutableList<State>
-                        if (list.size != 0)
-                            putMarkersOnMap(mMap, list)
-                        else
-                            Toast.makeText(applicationContext, "There is no flights with country: ${query}", Toast.LENGTH_SHORT).show()
-                    }else
-                        Toast.makeText(applicationContext, "Wrong query string", Toast.LENGTH_SHORT).show()
-                }
+                val params = filterPerform(query)
+                isFiltred = params.isFiltered
+                if (params.isFiltered) {
+                    mMap.clear()
+                    putMarkersOnMap(mMap, params.statesList)
+                } else
+                    Toast.makeText(applicationContext, params.statement, Toast.LENGTH_SHORT).show()
                 appearAnimation(searchView, searchButton)
-
-                searchView.setQuery("", false);
-                searchView.clearFocus()
                 return false
             }
 
@@ -99,25 +75,87 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private fun filterPerform(query: String): FilterParams {
+        var outMessage: String = ""
+        var list: MutableList<State> = mutableListOf()
+        var isFiltered: Boolean = false
+        query.trim()
+        filterQuery = query
+        if (query != "") {
+            var hasLetters = false
+            var hasNumbers = false
+            query.forEach {
+                if (it.isLetter())
+                    hasLetters = true
+                if (it.isDigit())
+                    hasNumbers = true
+            }
+            //Searching by flight number
+            if (hasLetters && hasNumbers) {
+                //mMap.clear()
+                list =
+                    cordsList.filter { it.callsign == query.toUpperCase() } as MutableList<State>
+                if (list.size != 0)
+                    isFiltered = true
+                else {
+                    outMessage = "There is no flights with number: ${query}"
+                    isFiltered = false
+                }
+            } else if (isCountry(query)) { //Searching by country
+                //mMap.clear()
+                list =
+                    cordsList.filter { it.origin_country.toLowerCase() == query.toLowerCase() } as MutableList<State>
+                if (list.size != 0)
+                    isFiltered = true
+                else {
+                    outMessage = "There is no flights with country: ${query}"
+                    isFiltered = false
+                }
+            } else {
+                outMessage = "Wrong query string"
+                isFiltered = false
+            }
+        }
+
+        searchView.setQuery("", false);
+        searchView.clearFocus()
+        return FilterParams(list, outMessage, isFiltered)
+    }
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         val wroclawCords = LatLng(51.1078852, 17.0385376)
         //putMarkersOnMap(mMap, cordsList)
         //Set center on Wroclaw
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(wroclawCords, 4.8f))
-        val start = CoroutinesToAPI(mMap, this)
+        //val start = CoroutinesToAPI(mMap, this)
         CoroutineScope(IO).launch {
-            start.coroutinesFetchJson()
+            coroutinesFetchJson()
 
+        }
+    }
+
+    suspend fun coroutinesFetchJson() {
+        println("Działa")
+        downloadJson()
+        delay(30000)
+        coroutinesFetchJson()
+    }
+
+    private suspend fun downloadJson() {
+        cordsList = JsonFetch.fetchJson()
+        if (isFiltred) {
+            val params = filterPerform(filterQuery)
+            setCordListInMainThread(params.statesList)
+        } else
+            setCordListInMainThread(cordsList)
+    }
+
+    private suspend fun setCordListInMainThread(list: MutableList<State>) {
+        withContext(Dispatchers.Main) {
+            //mMap.clear()
+            putMarkersOnMap(mMap, list)
         }
     }
 
@@ -140,12 +178,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     )
             )
             mMap.setInfoWindowAdapter(CustomInfoWindowForGoogleMap(this))
+            //delay(1)
         }
     }
 
-    fun allFlights(view: View){
+    fun allFlights(view: View) {
         mMap.clear()
-        putMarkersOnMap(mMap,cordsList)
+        putMarkersOnMap(mMap, cordsList)
+        isFiltred = false
+        filterQuery = ""
     }
 
     fun searchButtonAction(view: View) {
@@ -184,15 +225,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun isCountry(string: String) : Boolean{
+    private fun isCountry(string: String): Boolean {
         var isCountry = false
         val l = Locale.ENGLISH
         Locale.getISOCountries().forEach {
-            val locale = Locale("",it)
+            val locale = Locale("", it)
             val country = locale.getDisplayCountry(l)
             if (string.toLowerCase() == country.toLowerCase())
                 isCountry = true
         }
         return isCountry
     }
+}
+
+class FilterParams(
+    val statesList: MutableList<State>,
+    val statement: String,
+    val isFiltered: Boolean
+) {
+
 }
